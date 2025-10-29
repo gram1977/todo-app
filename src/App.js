b728d3693102eef8dd531c9d6fdc33ba2faca40f
aman.greval <aman.greval@gmail.com>
2025-10-29 19:29:12 +0530

    Add package.json, package-lock.json, /public/index.html, App.js, index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login/Login";
+import Welcome from "./components/Welcome/Welcome";
+import Menu from "./components/Menu/Menu";
+import Footer from "./components/Footer/Footer";
+// debugger;
+console.log("App.js loaded");
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  /*user = {
+  email: null,
+  password: null,
+};*/
+  const handleLogin = (userData) => {
+    setIsLoggedIn(true);
+    setUser(userData);
+    /*saves the user's email and password in the state variable user.
+    user = {
+  email: "abc@example.com",
+  password: "mypassword",
+};*/
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+    >
+      <Menu user={user} isLoggedIn={isLoggedIn} />
+      <main className="flex-grow-1">
+        {isLoggedIn ? (
+          // Render Welcome component when logged in
+          <Welcome user={user} />
+        ) : (
+          // Render Login component when not logged in
+          <Login onLogin={handleLogin} />
+          /*           onLogin={(email, password) => {
+                        console.log("Logging in with", email, password);
+                        // For now, just set logged in to true
+                        handleLogin({ email, password });
+                      }} */
+        )}
+      </main>
+      <Footer />
     </div>
+
+    // Conditional Rendering: Show different components based on Login status
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// debugger;
+console.log("index.js loaded");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
